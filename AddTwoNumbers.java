52e3ec4eb0da6cdce00f4a21750e78fdd99cd278
iharshit766 <harshi766766@gmail.com>
2025-02-27 11:26:43 +0530

    [Harshit] MODIFIED:Added Comments

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);	//Scanner Object
 
         System.out.print("Enter first number: ");
-        int num1 = sc.nextInt();
+        int num1 = sc.nextInt();		//Taking First Input
 
         System.out.print("Enter second number: ");
-        int num2 = sc.nextInt();
+        int num2 = sc.nextInt();		//Taking Second Input
 
-        int sum = num1 + num2;
+        int sum = num1 + num2;			//Addition Logic
 
-        System.out.println("The sum is: " + sum);
+        System.out.println("The sum is: " + sum);	//Result Printing
 
-        sc.close();
+        sc.close();				//Scanner Object Closing
     }
 }
